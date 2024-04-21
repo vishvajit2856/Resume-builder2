c656be1a2e800aff375cbeb5086dc7cb6eaad15d
vishvajit2856 <124611965+vishvajit2856@users.noreply.github.com>
2024-04-21 01:16:57 -0500

    Add files via upload
    some updation in the code

---
+++ Home_page.js
@@ -0,0 +1,21 @@
+function call_next_html_file(){
+    
+    window.location.href="user_info.html";
+    document.body.style.zoom = "80%";    
+}
+
+
+
+
+function call_next_html_file2(){
+    
+    window.location.href="expert_info.html";
+    document.body.style.zoom = "80%";    
+}
+
+function call_next_html_file3(){
+    
+    window.location.href="expert_info.html";
+    document.body.style.zoom = "80%";    
+
+}
